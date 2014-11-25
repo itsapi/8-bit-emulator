204c719088084180512bb14baea92bc3020202ff
Geraint White <geraintwhite@gmail.com>
2014-11-25 14:18:20 +0000

    WIP started implementing piping #2

--- assembler.js
+++ assembler.js
@@ -85,6 +85,8 @@ if (output) {
   var stream = fs.createWriteStream(output);
   stream.write(new Buffer(instructions));
   stream.end();
+} else if (!process.stdout.isTTY) {
+  process.stdout.write(new Buffer(instructions));
 } else {
   console.log(labels)
   console.log(instructions);
--- main.js
+++ main.js
@@ -163,18 +163,41 @@ RAM[0x01] = 0x05;
 RAM[0x02] = 0x51;
 RAM[0x03] = 0x15;
 
-if (input) {
-  var stream = fs.createReadStream(input);
-
-  var fd = fs.openSync(input, 'r');
-  var buf = new Buffer(1);
-
-  for (var i=0; fs.readSync(fd, buf, 0, 1) == 1; i++) {
-    RAM[i] = buf[0];
-  }
+var data = '';
+if (!process.stdin.isTTY) {
+  process.stdin.on('readable', function() {
+    var chunk = process.stdin.read();
+    if (chunk !== null) {
+      data += chunk;
+    }
+  });
+  process.stdin.on('end', function() {
+    run();
+  });
+} else {
+  run();
 }
 
-cpu();
+function run() {
+  if (input) {
+    var stream = fs.createReadStream(input);
 
-console.log(REG);
-console.log(RAM.slice(0, 20));
+    var fd = fs.openSync(input, 'r');
+    var buf = new Buffer(1);
+
+    for (var i=0; fs.readSync(fd, buf, 0, 1) == 1; i++) {
+      RAM[i] = buf[0];
+    }
+  } else if (data) {
+    var buf = new Buffer(data);
+    console.log(data)
+    for (i in buf) {
+      RAM[i] = buf[i];
+    }
+  }
+
+  // cpu();
+
+  // console.log(REG);
+  console.log(RAM.slice(0, 20));
+}
